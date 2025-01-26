f5e24f1ef37e9a929de41451303d2c2810f240ae
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-01-26 11:46:17 -0700

    Complete JavaScript Exercise 04
    Completed exercise 04 with all tests passing.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...args) {
+  for (const arg of [...args]) {
+    let index = arr.indexOf(arg);
+    while (index != -1) {
+      console.log(`${arg} found at index ${index} in ${arr}`);
+      arr.splice(index, 1);
+      index = arr.indexOf(arg);
+    }
+  }
+  return arr;
 };
 
 // Do not edit below this line
