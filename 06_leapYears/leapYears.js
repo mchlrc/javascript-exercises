c53309693a772bbd3ed6d7d5d2e6826069b3d041
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-01-26 12:30:43 -0700

    Complete JavaScript Exercise 06
    Completed exercise 06 with all tests passing.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,11 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  result = false;
+  if (year % 100 === 0 && year % 400 === 0) {
+    result = true;
+  } else if (year % 100 !== 0 && year % 4 === 0) {
+    result = true;
+  }
+  return result;
 };
 
 // Do not edit below this line
