5f8e1584b129b1375e3f2cc76701ec5dae8543de
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-01-26 12:15:00 -0700

    Complete JavaScript Exercise 05
    Completed exercise 05 with all tests passing.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,29 @@
-const sumAll = function() {
+const sumAll = function (start, end) {
+  let sum = "ERROR";
+  console.log(start);
+  console.log(end);
+  console.log(typeof start);
+  console.log(typeof end);
+  console.log(Number.isInteger(start));
+  console.log(Number.isInteger(end));
 
+  if (typeof start === "number" && typeof end === "number") {
+    if (
+      Number.isInteger(start) &&
+      Number.isInteger(end) &&
+      start >= 0 &&
+      end >= 0
+    ) {
+      if (start > end) {
+        [start, end] = [end, start];
+      }
+      sum = 0;
+      for (let i = start; i <= end; i++) {
+        sum += i;
+      }
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
