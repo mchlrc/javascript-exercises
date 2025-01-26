0f7e01dfebb39ed3c3fd8dbd2e811fbdd614d78d
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-01-26 10:40:00 -0700

    Complete JavaScript Exercise 03
    Completed exercise 03 with all tests passing.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let temp = "";
+  for (let i = 0; i < string.length; i++) {
+    temp = temp.concat(string.at(string.length - i - 1));
+  }
+  return temp;
 };
 
 // Do not edit below this line
