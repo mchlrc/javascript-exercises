7b613588f28c910f0a5542f29630d80549340397
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-01-26 10:01:16 -0700

    Complete JavaScript Exercise 02
    Completed exercise 02 with all tests passing.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let temp = "";
+  if (num < 0) {
+    temp = "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      temp = temp.concat(string);
+    }
+  }
+  return temp;
 };
 
 // Do not edit below this line
