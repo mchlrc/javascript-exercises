e5664c800dc7b7e7e6e4b8f216e4a72a26332093
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-02-04 18:02:00 -0700

    Complete Exercise 09
    Completed JavaScript exercise 09 with all tests
    passing.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  str = str.split("").filter((c) => !["!", ".", "?", ",", " "].includes(c));
+  let rev = str.slice();
+  rev = rev.reverse().join("").toLowerCase();
+  str = str.join("").toLowerCase();
+  console.log(`${str} === ${rev}`);
+  return str === rev;
 };
 
 // Do not edit below this line
