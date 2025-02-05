79d14e60fda36de1904e839533d31081a1a3a002
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-02-04 20:38:24 -0700

    Complete Exercise 12
    Completed JavaScript exercise 12 with all tests
    passing.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function (people) {
+  let modified = people.map((person) => {
+    let today = new Date().getFullYear();
+    console.log({ today });
+    if (person.yearOfDeath === undefined) {
+      person.yearOfDeath = today;
+    }
+    return person;
+  });
 
+  let sorted = modified.sort(
+    (a, b) => b.yearOfDeath - b.yearOfBirth - (a.yearOfDeath - a.yearOfBirth)
+  );
+  console.log({ sorted });
+  console.log({ modified });
+  return sorted[0];
 };
 
 // Do not edit below this line
