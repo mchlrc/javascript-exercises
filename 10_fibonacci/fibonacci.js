93d2f05866b020ba67c45b32f844094207278e47
mchlrc <108034624+mchlrc@users.noreply.github.com>
2025-02-04 19:00:54 -0700

    Complete Exercise 10
    Completed JavaScript Exercise 10 with all tests
    passing.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function (member) {
+  member = Number(member);
+  let defaults = [0, 1, 1];
+  let count = 2;
+  let previous = 0;
+  let current = 1;
+  let numbers = [1, 1];
 
+  if (member < 3 && member >= 0) {
+    return defaults[member];
+  } else if (member < 0) {
+    return "OOPS";
+  }
+  while (count < member) {
+    numbers.push(numbers.at(-1) + numbers.at(-2));
+    console.log(numbers);
+    numbers.shift(1);
+    count++;
+  }
+  return numbers.at(-1);
 };
 
 // Do not edit below this line
